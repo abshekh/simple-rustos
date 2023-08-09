3561ec7bdf8e46f2d1bdab0a9ba8415faa1e5ab2
Abhishek Singh <abhishek17513@gmail.com>
2023-08-09 16:22:05 +0530

    add testing framework

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,41 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
-
-mod vga_buffer;
+#![feature(custom_test_frameworks)] // default test framework is tied to no_std
+#![test_runner(simple_rustos::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
-/// This function is called on panic.
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
+use simple_rustos::println;
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
     println!("Hello world{}", "!");
-    panic!("panic message");
+    // panic!("panic message");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+// our panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    simple_rustos::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
